e578860651221c67d555f0031b4e16fb42e50edb
Vuongg <nguyenvuong7411@gmail.com>
2026-05-16 10:50:25 +0700

    Add EDA notebooks and plots

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print ('hehehehe')
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
+# Load the dataset
+
+# Assuming the dataset is in a CSV file named 'data.csv'
+data = pd.read_csv('data.csv')
+
